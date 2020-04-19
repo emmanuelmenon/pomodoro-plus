9e330c7dc7c4ac158b96b64fd30e0934b4f0e748
Emmanuel Menon <menon281004@gmail.com>
2020-04-20 03:14:33 +1000

    basic functionality implemented

---
+++ spec/pomodoro-plus-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PomodoroPlusView from '../lib/pomodoro-plus-view';
+
+describe('PomodoroPlusView', () => {
+    it('has one valid test', () => {
+        expect('life').toBe('easy');
+    });
+});
